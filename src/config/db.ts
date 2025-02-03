06634ec035ec07b2032e9cd25f786242805c8cdc
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-02-03 16:52:42 +0530

    add change

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+    await mongoose.connect(process.env.MONGO_URI as any);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
