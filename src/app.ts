a3f39d3f3d9726414bb6ae40d25bbdc8934f6e6e
Nivedita <nivedita.upadhayay@aayaninfotech.com>
2025-03-20 18:01:04 +0530

    fix cors issue

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ connectDB();
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://3.223.253.106:2564',
+    credentials: true
+}));
 
 app.use("/", routes);
 
