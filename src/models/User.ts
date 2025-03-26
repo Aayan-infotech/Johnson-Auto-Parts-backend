578e2c7ad0e0ed3e94cae6c22e89bb3bc8374522
anurag721721 <anurag.yadav@aayaninfotech.com>
2025-03-26 10:51:57 +0530

    add change

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const UserSchema = new Schema<IUser>(
   {
+    name: { type: String, required: true },
     userId: {
       type: String,
       required: true,
