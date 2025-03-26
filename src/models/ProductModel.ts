194eb22d275e767866a33732accc631af38e8c82
anurag721721 <anurag.yadav@aayaninfotech.com>
2025-03-26 17:59:32 +0530

    added french language for product ad well

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -1,22 +1,25 @@
 import mongoose, { Schema } from "mongoose";
 import { IProduct } from "./interfaces/IProduct"; // Importing the interface
 
-const ProductSchema = new Schema<IProduct>({
-    Category:{ type: String, required: true },
-    SubCategory:{ type: String, },
-    SubSubcategory: { type: String, },
-    name: { type: String, required: true, },
-    description: { type: String, required: true },
+const ProductSchema = new Schema<IProduct>(
+  {
+    Category: { type: String, required: true },
+    SubCategory: { type: String },
+    SubSubcategory: { type: String },
+    name: { en: { type: String, required: true }, fr: { type: String } },
+    description: { en: { type: String, required: true }, fr: { type: String } },
     price: {
-        actualPrice: { type: Number, required: true },
-        discountPercent: { type: Number, required: true, default: 0 }
+      actualPrice: { type: Number, required: true },
+      discountPercent: { type: Number, required: true, default: 0 },
     },
-    brand: { type: String, required: true },
+    brand: { en: { type: String, required: true }, fr: { type: String } },
     picture: { type: [String], required: true },
-    quantity: { type: Number, required: true, default: 0 }, 
-    isActive: { type: Boolean, required: true, default: true } 
-}, {
-    timestamps: true
-});
+    quantity: { type: Number, required: true, default: 0 },
+    isActive: { type: Boolean, required: true, default: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model<IProduct>("Product", ProductSchema);
