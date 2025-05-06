e9203ad6c2b1eac619430a5c07f24710d63ae130
Nivedita <nivedita.upadhayay@aayaninfotech.com>
2025-05-06 10:20:47 +0530

    Add changes for the user fetch

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -253,16 +253,16 @@ const getUserDetails = async (req: AuthRequest, res: Response) => {
 };
 // Get All Users with Pagination
 const getAllUsers = asyncHandler(async (req: Request, res: Response) => {
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
+  // const page = Number(req.query.page) || 1;
+  // const limit = Number(req.query.limit) || 10;
+  // const skip = (page - 1) * limit;
 
   const totalUsers = await User.countDocuments();
   const users = await User.find()
     .select("-password")
-    .skip(skip)
-    .limit(limit)
-    .lean();
+    // .skip(skip)
+    // .limit(limit)
+    // .lean();
 
   if (users.length === 0) {
     res.status(404).json({
@@ -277,9 +277,9 @@ const getAllUsers = asyncHandler(async (req: Request, res: Response) => {
     success: true,
     status: 200,
     message: "Users fetched successfully!",
-    currentPage: page,
-    totalPages: Math.ceil(totalUsers / limit),
-    totalUsers,
+    // currentPage: page,
+    // totalPages: Math.ceil(totalUsers / limit),
+    // totalUsers,
     data: users,
   });
 });
