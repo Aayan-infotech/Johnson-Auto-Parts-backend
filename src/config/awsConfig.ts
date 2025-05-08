f1a83243d64a7026a1ab926c89428023628d48f4
anurag721721 <anurag.yadav@aayaninfotech.com>
2025-05-08 11:37:56 +0530

    secret edit

--- src/config/awsConfig.ts
+++ src/config/awsConfig.ts
@@ -13,10 +13,10 @@ const secretsManagerClient = new SecretsManagerClient({
 // Fetch AWS credentials from Secrets Manager
 export const getAwsCredentials = async (): Promise<{ accessKeyId: string; secretAccessKey: string }> => {
   try {
-    const command = new GetSecretValueCommand({ SecretId: 'aws-secret' });
+    const command = new GetSecretValueCommand({ SecretId: 'john4' });
     const data = await secretsManagerClient.send(command);
 
-    if (data.SecretString) {
+    if (data.SecretString) { 
       const secret = JSON.parse(data.SecretString);
       return {
         accessKeyId: secret.AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID!,
